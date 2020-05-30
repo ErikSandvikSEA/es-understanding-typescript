a842d26b71bc4d7c44b3eaa29e61d0d4e4bf7ed2
eriksandviksea <eriksandvikSEA@Gmail.com>
2020-05-29 21:00:56 -0700

    added union-aliases.ts

--- basics-05-object-types/app.ts
+++ basics-05-object-types/app.ts
@@ -1,25 +1,18 @@
-function combine(
-	input1: number | string,
-	input2: number | string,
-	resultConversion: 'as-number' | 'as-text'
-) {
-	let result
-	if (
-		(typeof input1 === 'number' && typeof input2 === 'number') ||
-		resultConversion === 'as-number'
-	) {
-		result = +input1 + +input2
-	} else {
-		result = input1.toString() + input2.toString()
-	}
-	return result
+function add(n1: number, n2: number) {
+	return n1 + n2
 }
 
-const combinedAges = combine(30, 26, 'as-number')
-console.log(combinedAges) //prints 56
+function printResult(num: number) {
+	console.log('Result: ' + num)
+}
+printResult(add(5, 12))
 
-const combinedStringAges = combine('30', '25', 'as-number')
-console.log(combinedStringAges) //prints 55
+let combineValues: (a: number, b: number) => number
 
-const combinedNames = combine('Max', 'Anna', 'as-text')
-console.log(combinedNames) //prints MaxAnna
+combineValues = add
+
+// combineValues = 5
+// combineValues = 'COMBO!'
+// combineValues = printResult
+
+console.log(combineValues(8, 8))
